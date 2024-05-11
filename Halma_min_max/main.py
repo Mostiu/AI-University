4ba435b13f2f21c1f69998add97a7e26144f107f
Tomasz Koralewski <122738616+Mostiu@users.noreply.github.com>
2024-05-11 22:42:53 +0200

    Update main.py

--- Halma_min_max/main.py
+++ Halma_min_max/main.py
@@ -1,93 +1,131 @@
+from copy import deepcopy
 from game import Board
-from heuristics import Heuristics
+import random
 from time import time
+from strategies import Heuristics
+from minmax import minimax, alfabeta
+from const import PLAYER1_START_POSITIONS, PLAYER2_START_POSITIONS
 
 
-def minimax(board: Board, depth: int, maximizing: bool, player: int, eval_func) -> float:
-    is_over = board.is_game_over()
-    if is_over:
-        return (float('+inf'), None) if is_over == player else (float('-inf'), None)
+def get_max_pos_moves():
+    b = Board()
+    no_pos_moves = []
+    for _ in range(10000):
+        pos_moves = b.get_all_possible_moves()
+        if not pos_moves:
+            raise ValueError('No possible moves')
 
-    if depth == 0:
-        return (eval_func(board, player), None)
+        no_pos_moves.append(sum(len(value) for value in pos_moves.values()))
 
-    if maximizing:
+        for (row, col), moves in pos_moves.items():
+            if len(moves) != len(set(moves)):
+                raise ValueError('Duplicate moves')
+
+        key = random.choice(list(pos_moves.keys()))
+        move = random.choice(pos_moves[key])
+        b.move_pawn(key[0], key[1], move[0], move[1])
+
+    return max(no_pos_moves)
+
+
+def test_distance_values():
+    b = Board()
+    print(Heuristics.dist_from_oponent_corner(b, 1))
+
+    for pos in PLAYER1_START_POSITIONS:
+        b.board[pos[0]][pos[1]] = 2
+
+    for pos in PLAYER2_START_POSITIONS:
+        b.board[pos[0]][pos[1]] = 1
+        print(Heuristics.dist_from_oponent_corner(b, 1))
+    print(b)
+    print(b.is_game_over())
+
+    print(Heuristics.dist_from_oponent_corner(b, 1))
+
+
+def test_simple_distance_values():
+    b = Board()
+    print(Heuristics.simple_distance(b, 1))
+
+    for pos in PLAYER1_START_POSITIONS:
+        b.board[pos[0]][pos[1]] = 2
+
+    for pos in PLAYER2_START_POSITIONS:
+        b.board[pos[0]][pos[1]] = 1
+        print(Heuristics.simple_distance(b, 1))
+
+    print(Heuristics.simple_distance(b, 1))
+
+
+def test_endgame_values():
+    b = Board()
+    print(Heuristics.no_pawns_at_finish(b, 2))
+
+    for pos in PLAYER2_START_POSITIONS:
+        b.board[pos[0]][pos[1]] = 1
+
+    for pos in PLAYER1_START_POSITIONS:
+        b.board[pos[0]][pos[1]] = 2
+        print(Heuristics.no_pawns_at_finish(b, 2))
+
+    print(Heuristics.no_pawns_at_finish(b, 2))
+
+
+def simulate_game(heuristic, depth=3):
+    board = Board()
+    turn_num = 1
+    while not board.is_game_over():
+        print(f'Turn: {int(turn_num)}, Player: {board.turn}')
+        player = board.turn
         best_move = None
-        max_eval = -float('inf')
-        for pawn, move in board.get_all_possible_moves().items():
-            # pawn (row, col), move [(new_row, new_col), ...]
-            for new_row, new_col in move:
-                if not best_move:
-                    best_move = (pawn, (new_row, new_col))
-                new_board = Board(board.board, board.turn)
-                new_board.move_pawn(pawn[0], pawn[1], new_row, new_col)
-                eval = minimax(new_board, depth - 1,
-                               False, player, eval_func)[0]
-                if eval > max_eval:
-                    max_eval = eval
-                    best_move = (pawn, (new_row, new_col))
-        return max_eval, best_move
+        best_move = alfabeta(
+            board, 1, True, player, heuristic)
+        print(board)
+        board.move_pawn(*best_move[1][0], *best_move[1][1])
+        turn_num += 0.5
 
-    else:
-        best_move = None
-        min_eval = float('inf')
-        for pawn, move in board.get_all_possible_moves().items():
-            for new_row, new_col in move:
-                if not best_move:
-                    best_move = (pawn, (new_row, new_col))
-                new_board = Board(board.board, board.turn)
-                new_board.move_pawn(pawn[0], pawn[1], new_row, new_col)
-                eval = minimax(new_board, depth - 1,
-                               True, player, eval_func)[0]
-                if eval < min_eval:
-                    min_eval = eval
-                    best_move = (pawn, (new_row, new_col))
-
-        return min_eval, best_move
+    print(board)
+    print('\n\n----------------------------')
+    print(f'Game won by: {board.is_game_over()}')
 
 
-def alfabeta(board: Board, depth: int, maximizing: bool, player: int, eval_func, alfa=float('-inf'), beta=float('+inf')) -> float:
-    is_over = board.is_game_over()
-    if is_over:
-        return (float('+inf'), None) if is_over == player else (float('-inf'), None)
+def simulate_game_adaptive(player1_heuristics, player2_heuristics, depth=2):
+    board = Board()
+    turn_num = 1
+    while not board.is_game_over():
+        print(f'Turn: {int(turn_num)}, Player: {board.turn}')
+        player = board.turn
+        if player == 1:
+            keys = sorted(key for key in player1_heuristics.keys()
+                          if key > turn_num)
+            heuristic = player1_heuristics[keys[0]
+                                           ] if keys else player1_heuristics[list(player1_heuristics.keys())[-1]]
+            best_move = alfabeta(
+                board, 1, True, player, heuristic)
+        else:
+            keys = sorted(key for key in player2_heuristics.keys()
+                          if key > turn_num)
+            heuristic = player2_heuristics[keys[0]
+                                           ] if keys else player2_heuristics[list(player2_heuristics.keys())[-1]]
+            best_move = alfabeta(
+                board, depth, True, player, heuristic)
+        print(board)
+        board.move_pawn(*best_move[1][0], *best_move[1][1])
+        turn_num += 0.5
 
-    if depth == 0:
-        return (eval_func(board, player), None)
+    print(board)
+    print('\n\n----------------------------')
+    print(f'Game won by: {board.is_game_over()}')
 
-    if maximizing:
-        best_move = None
-        max_eval = -float('inf')
-        for pawn, move in board.get_all_possible_moves().items():
-            for new_row, new_col in move:
-                if not best_move:
-                    best_move = (pawn, (new_row, new_col))
-                new_board = Board(board.board, board.turn)
-                new_board.move_pawn(pawn[0], pawn[1], new_row, new_col)
-                eval = alfabeta(new_board, depth - 1, False,
-                                player, eval_func, alfa, beta)[0]
-                if eval > max_eval:
-                    max_eval = eval
-                    best_move = (pawn, (new_row, new_col))
-                alfa = max(alfa, eval)
-                if beta <= alfa:
-                    break
-        return max_eval, best_move
 
-    else:
-        best_move = None
-        min_eval = float('inf')
-        for pawn, move in board.get_all_possible_moves().items():
-            for new_row, new_col in move:
-                if not best_move:
-                    best_move = (pawn, (new_row, new_col))
-                new_board = Board(board.board, board.turn)
-                new_board.move_pawn(pawn[0], pawn[1], new_row, new_col)
-                eval = alfabeta(new_board, depth - 1, True,
-                                player, eval_func, alfa, beta)[0]
-                if eval < min_eval:
-                    min_eval = eval
-                    best_move = (pawn, (new_row, new_col))
-                beta = min(beta, eval)
-                if beta <= alfa:
-                    break
-        return min_eval, best_move
+if __name__ == '__main__':
+    # simulate_game(Heuristics.complex, 1)
+    p1_heuristics = {50: Heuristics.random, 200: Heuristics.complex}
+    p2_heuristics = {1: Heuristics.complex}
+    simulate_game_adaptive(p2_heuristics, p2_heuristics, 2)
+    # print(board)
+    # print(board.is_game_over())
+    # test_distance_values()
+    # test_endgame_values()
+    # test_simple_distance_values()
